007167ac2c6a4ed872607a83ba15d06b56a9f1b7
seo yunjoo <fridaymorning99@naver.com>
2026-01-27 13:59:03 +0930

    static - manage the list common

--- CoffeeApi/Controllers/CoffeeController.cs
+++ CoffeeApi/Controllers/CoffeeController.cs
@@ -8,44 +8,25 @@ namespace CoffeeApi.Controllers
     [ApiController]
     public class CoffeeController : ControllerBase
     {
-        // [GET] 요청이 들어오면 실행되는 함수
-        [HttpGet]
-        public List<Coffee> GetCoffeeMenu()
-        {
-            List<Coffee> coffeeList = new List<Coffee>
+        // static을 붙여서 서버가 켜져 있는 동안 리스트를 딱 하나만 유지
+        private static List<Coffee> coffeeList = new List<Coffee>
             {
                 new Coffee("아메리카노", 4500),
                 new Coffee("카페라떼", 5000),
                 new Coffee("바닐라라떼", 5500)
             };
 
-            return coffeeList;
-            
-            /*
-            // 간단한 메뉴 리스트를 만들어 반환
-            List<string> menu = new List<string>
-            {
-                "에스프레소",
-                "아메리카노",
-                "카페라떼",
-                "바닐라라떼 (추천)"
-            };
-
-            return menu;
-            */
+        // [GET] 요청이 들어오면 실행되는 함수
+        [HttpGet]
+        public List<Coffee> GetCoffeeMenu()
+        {
+            return coffeeList; // 공통 리스트 반환
         }
 
         // 특정 순번(index)의 커피 정보를 반환하는 함수
         // 주소 예시 : api/coffee/0 또는 api/coffee/1
         [HttpGet("{id}")]
         public Coffee GetCoffeeById(int id) {             // 간단한 예시로, 고정된 커피 데이터를 반환
-            List<Coffee> coffeeList = new List<Coffee>
-            {
-                new Coffee("아메리카노", 4500),
-                new Coffee("카페라떼", 5000),
-                new Coffee("바닐라라떼", 5500)
-            };
-
             // 만약 리스트 범위 밖의 숫자를 요청하면 에러 발생 가능, 안전장치 설치
             if (id < 0 || id >= coffeeList.Count)
             {
